524e372cd21f7bbc4d607c4f61431f1bec8fab1d
mngarwood <megan@megangarwood.com>
2023-02-06 16:15:14 -0600

    Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,53 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const SelectWrapper = styled.div`
+  position: relative;
+`
+
+const FauxSelect = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  border-radius: 8px;
+  height: 43px;
+  width: fit-content;
+  display: flex;
+  align-items: center;
+  padding: 0 16px;
+  pointer-events: none;
+`
+
+const RealSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+
+  &:focus + ${FauxSelect} {
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  &:hover + ${FauxSelect} {
+    color: ${COLORS.black};
+  }
+`
+
+const MyIcon = styled(Icon)`
+  margin-left: 20px;
+`
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <SelectWrapper>
+      <RealSelect value={value} onChange={onChange}>
+        {children}
+      </RealSelect>
+      <FauxSelect>{displayedValue} <MyIcon id="chevron-down" size={24} strokeWidth={2} /></FauxSelect>
+    </SelectWrapper>
   );
 };
 
